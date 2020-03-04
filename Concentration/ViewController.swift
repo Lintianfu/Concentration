a07675afd620e68515df922611d70801543eb4f6
LINTian <986488402@qq.com>
2020-03-04 22:51:57 +0800

    提交swift第一个翻牌小游戏，设计到的知识点：可以在button里面设置title内容为emoji表情，创建多个按钮，设置为collection模式，直接复制按钮的话，会将方法也一起复制了。通过viewVC上面的黄点进行连线，可以使得多个button触发同一个方法。

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var CardButtons: [UIButton]!
+    @IBOutlet weak var flipCountLabel: UILabel!
+    var flipCount:Int=0
+       {
+           didSet
+           {
+               flipCountLabel.text="FlipCount:\(flipCount)"
+           }
+       }
+    var emojis=["🦊","🙈","🐮","🦊","🙈","🐮"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount+=1
+             if let cardNumber=CardButtons.firstIndex(of:sender)
+             {
+                   flipCard(withEmoji: emojis[cardNumber], on: sender)
+             }else
+             {
+                    print("show error")
+             }
+    }
+    func flipCard(withEmoji emoji:String,on button:UIButton) -> Void {
+        if button.currentTitle==emoji
+        {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor=#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor=#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+       
+    }
+    
 }
 
